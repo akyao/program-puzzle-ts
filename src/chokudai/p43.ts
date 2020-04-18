bd1bd5b492fd3fdcb215c19597d4a26baec6ff4b
konuma <konama.akyao@gmail.com>
2020-04-18 14:35:31 +0900

    chokudai/p43

--- src/chokudai/p43.ts
+++ src/chokudai/p43.ts
@@ -1,15 +1,25 @@
 /**
  * キウイジュース
  */
-
-namespace choku {
-    function thePouring(capacities: Array<number>,
+export default function thePouring(capacities: Array<number>,
                         bottles: Array<number>,
                         fromId: Array<number>,
                         toId: Array<number>): Array<number> 
-    {
-        let resultBottles: Array<number> = [];
+{
+    let resultBottles: Array<number> = bottles;
+    for (let i = 0; i < fromId.length; i++) {
+        // fromから注げる最大値
+        const fromMax = resultBottles[fromId[i]];
+        // toへ注げる最大値
+        const toMax = capacities[toId[i]] - resultBottles[toId[i]];
+        // 注ぐ量
+        const moveAmount: number = Math.min(fromMax, toMax);
 
-        return resultBottles;
+        // 注ぐ
+        resultBottles[fromId[i]] = resultBottles[fromId[i]] - moveAmount;
+        resultBottles[toId[i]] = resultBottles[toId[i]] + moveAmount;
     }
+    
+    return resultBottles;
 }
+
